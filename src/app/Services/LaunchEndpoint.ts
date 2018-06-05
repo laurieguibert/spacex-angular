8386fc16404cfa8070d42eb0ab07437acb776959
DeschampsManon <deschamps.manon.91@gmail.com>
2018-06-05 17:17:02 +0200

    create navigation component with links in params #5

--- src/app/Services/LaunchEndpoint.ts
+++ src/app/Services/LaunchEndpoint.ts
@@ -1,21 +1,18 @@
-export enum LaunchEndpoints { 
-    /**
-     * Get latest launch
-     */
-    Latest = 0,
-
-    /**
-     * Get next launch
-     */
-    Next = 1,
-
-    /**
-     * Get all upcoming launches
-     */ 
-    Upcoming = 2,
-
-    /**
-     * Get all upcoming and past launches
-     */
-    All = 3,
+export enum LaunchEndpoints {
+/**
+ * Get latest launch
+ */
+Latest = 0,
+/**
+ * Get next launch
+ */
+Next = 1,
+/**
+ * Get all upcoming launches
+ */
+Upcoming = 2,
+/**
+ * Get all upcoming and past launches
+ */
+All = 3,
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class NavbarComponent implements OnInit {
+  @Input() links;
   constructor() {
   }
   ngOnInit() {
