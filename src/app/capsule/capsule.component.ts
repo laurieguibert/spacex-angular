05c59ee10142eba4b59c396d5f13b412d6053428
laurieguibert <l.guibert@hotmail.fr>
2018-06-06 11:03:20 +0200

    Generate capsules endpoint #7

---
+++ src/app/capsule/capsule.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SpacexApiService } from 'src/app/Services/spacex-api.service';
+
+@Component({
+  selector: 'app-capsule',
+  templateUrl: './capsule.component.html',
+  styleUrls: ['./capsule.component.scss']
+})
+export class CapsuleComponent implements OnInit {
+  capsules : Capsule[];
+  capsule : Capsule;
+
+  constructor(private spacexApi: SpacexApiService) {
+    this.spacexApi.getAllCapsules().subscribe((data : Capsule[]) => {
+      this.capsules = data;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  loadDetailsCapsule(capsuleId : string){
+    this.spacexApi.getCapsule(capsuleId).subscribe((data : Capsule) => {
+      this.capsule = data;
+    });
+  }
+}
