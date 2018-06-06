19ab3a6a69919420c2e48d11794d464806ed106d
DeschampsManon <deschamps.manon.91@gmail.com>
2018-06-06 12:30:04 +0200

    change navigation link contents #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,27 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'app';
   links = [
-    {name: 'Falcon 9', component: '', href: '/#!/'},
-    {name: 'Falcon Heavy', component: '', href: '/#!/'},
-    {name: 'Dragon', component: '', href: '/#!/'},
-    {name: 'Updates', component: '', href: '/#!/'},
-    {name: 'About SpaceX', component: '', href: '/#!/'},
-    {name: 'Careers', component: '', href: '/#!/'},
-    {name: 'Gallery', component: '', href: '/#!/'},
-    {name: 'Home', component: '', href: 'home'},
+    {
+      name: 'Falcon 9',
+      href: '/#!/'
+    },
+    {
+      name: 'Falcon Heavy',
+      href: '/#!/'
+    },
+    {
+      name: 'Dragon',
+        href: '/#!/'
+    },
+    {
+      name: 'Updates',
+        href: '/#!/'
+    },
+    {
+      name: 'About SpaceX',
+        href: '/#!/'
+    }
   ];
 }
 
