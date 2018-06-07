bfe3c37d55b724c66f49fb049da509119c3eefdd
DeschampsManon <deschamps.manon.91@gmail.com>
2018-06-07 10:52:21 +0200

    setup carousel component params #14

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  @Input() items;
 
   constructor() { }
 
