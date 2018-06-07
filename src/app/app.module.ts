6142c399541c25d90c1823054658a5d378aaea02
laurieguibert <l.guibert@hotmail.fr>
2018-06-07 20:21:25 +0200

    integrate Google Maps and materialize #20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,12 @@ import { NgHttpLoaderModule } from 'ng-http-loader';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-
 import { HomeComponent } from './pages/home/home.component';
 import { RocketComponent } from './pages/rocket/rocket.component';
-
 import { MissionsComponent } from './missions/missions.component';
 import { LaunchComponent } from './launch/launch.component';
 import { CompanyInfoComponent } from './pages/company-info/company-info.component';
 import { CompanyHistoryComponent } from './pages/company-history/company-history.component';
-
 import { CapsuleComponent } from './capsule/capsule.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { RocketListComponent } from './rocket-list/rocket-list.component';
@@ -25,6 +22,8 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { TabComponent } from './tab/tab.component';
 import { AboutComponent } from './pages/about/about.component';
 import { MissionsDetailsComponent } from './missions-details/missions-details.component';
+import { AgmCoreModule } from '@agm/core';
+import { LaunchpadComponent } from './launchpad/launchpad.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +41,8 @@ import { MissionsDetailsComponent } from './missions-details/missions-details.co
     CarouselComponent,
     TabComponent,
     AboutComponent,
-    MissionsDetailsComponent
+    MissionsDetailsComponent,
+    LaunchpadComponent
   ],
   imports: [
     BrowserModule,
@@ -52,7 +52,10 @@ import { MissionsDetailsComponent } from './missions-details/missions-details.co
     NgHttpLoaderModule,
     AngularFontAwesomeModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAa2wNa1V87VMlzkV65KevyZJfmMyIEiAA'
+    })
   ],
   providers: [],
   bootstrap: [ AppComponent ],
--- src/app/missions-details/missions-details.component.ts
+++ src/app/missions-details/missions-details.component.ts
@@ -9,11 +9,17 @@ import { SpacexApiService } from 'src/app/Services/spacex-api.service';
 })
 export class MissionsDetailsComponent implements OnInit {
   launch: Launch;
+  launchpad: Launchpad;
 
   constructor(private spacexApi: SpacexApiService,
-    private route: ActivatedRoute) { 
+    private route: ActivatedRoute) {
     const flight_number = this.route.snapshot.params.flight_number;
-    this.spacexApi.getLaunchDetails(flight_number).subscribe(data => this.launch = data);
+    this.spacexApi.getLaunchDetails(flight_number).subscribe(data => {
+      this.launch = data;
+      this.spacexApi.getLaunchpad(this.launch[0].launch_site.site_id).subscribe(data => {
+        this.launchpad = data;
+      });
+    });
   }
 
   ngOnInit() {
